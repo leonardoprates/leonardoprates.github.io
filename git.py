c4cc03acc112753b3ba228d539d72a146a20efec
odran037 <odran037@users.noreply.github.com>
2015-07-12 22:37:56 -0700

    commit -m Update

--- git.py
+++ git.py
@@ -8,10 +8,10 @@ def git(choice):
 
   if choice == '1':
       option += raw_input('Enter comment: ')
-      print option
+      print option, '\n'
   else:
       option += 'Update'
-      print option
+      print option, '\n'
 
   call(['git', 'add', '*'])
   call(['git', 'commit', '-m', option])
