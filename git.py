ab5b71f1efa189a66675520d5fa41a944e0ae19c
odran037 <odran037@users.noreply.github.com>
2015-07-12 17:15:22 -0700

    Update

--- git.py
+++ git.py
@@ -1,5 +1,5 @@
 from subprocess import call
 call(["git", "add", "*"])
-comment = raw_input("Enter commit comment:")
+comment = raw_input("Enter commit comment: ")
 call(["git", "commit", "-m", comment])
 call(["git", "push", "origin", "master"])
