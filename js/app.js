bcf7a482687af1370db199fd30b75c46b33e6045
odran037 <odran037@users.noreply.github.com>
2016-01-16 22:53:12 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -14,13 +14,13 @@ var About = React.createClass({
     return (
       <section className="text-center">
         <p className="caps">Hello! My name is Leo. I make things with code. I also like wood working and bicyles.</p>
-         <div className="icons">
-          <a href="#"><i className="fa fa-github fa-3x"></i></a>
-          <a href="#"><i className="fa fa-codepen fa-3x"></i></a>
-          <a href="#"><i className="fa fa-linkedin fa-3x"></i></a>
-          <a href="#"><i className="fa fa-twitter fa-3x"></i></a>
-          <a href="#"><i className="fa fa-envelope fa-3x"></i></a>
-          <a href="#"><i className="fa fa-file-text-o fa-3x"></i></a>
+        <div className="icons">
+          <a href="https://github.com/odran037" target="_blank"><i className="fa fa-github fa-3x"></i></a>
+          <a href="https://codepen.io/odran037" target="_blank"><i className="fa fa-codepen fa-3x"></i></a>
+          <a href="https://www.linkedin.com/in/odran037" target="_blank"><i className="fa fa-linkedin fa-3x"></i></a>
+          <a href="https://twitter.com/odran037" target="_blank"><i className="fa fa-twitter fa-3x"></i></a>
+          <a href="mailto:hello@odran037.io" target="_blank"><i className="fa fa-envelope fa-3x"></i></a>
+          <a href="resume/index.html" target="_blank"><i className="fa fa-file-text-o fa-3x"></i></a>
         </div>
       </section>
     );
@@ -33,7 +33,7 @@ var Footer = React.createClass({
       <footer className="col-lg-8 col-lg-offset-2">
         <p>
           <span className="sm-caps">leonardo prates </span>
-          <i className="fa fa-gears"></i>
+          <i className="fa fa-gears"></i>&nbsp;
           <span className="date">2015</span> |
           <span className="sm-caps"> built with </span>
           <img src="http://red-badger.com/blog/wp-content/uploads/2015/04/react-logo-1000-transparent.png" alt="ReactJS" width="25px" />
