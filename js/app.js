77519d83d129646c6fef1aafb1882adf7e287cc0
odran037 <odran037@users.noreply.github.com>
2016-01-16 22:38:01 -0600

    Update

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ var About = React.createClass({
 var Footer = React.createClass({
   render: function() {
     return (
-      <footer className="col-lg-8 col-ld-offset-2">
+      <footer className="col-lg-8 col-lg-offset-2">
         <p>
           <span className="sm-caps">leonardo prates </span>
           <i className="fa fa-gears"></i>
