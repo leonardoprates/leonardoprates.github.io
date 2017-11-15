0302e9e1832434012a5f0eee5b78866b2e2f8fcb
odran037 <leonardo.prates@outlook.com>
2017-11-14 23:18:59 -0600

    :bike:

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-const label = 'leonardo prates';
-const head = document.head;
-const title = document.createElement('title');
-const titleText = document.createTextNode(label);
-const link = document.createElement('link');
-const body = document.body;
-const h1 = document.createElement('h1');
-const a = document.createElement('a');
-const h1Styles = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  textAlign: 'center'
-};
-const aStyles = {
-  color: '#000000',
-  textDecoration: 'none',
-  borderBottom: '5px dotted #000',
-  fontFamily: 'monospace',
-  fontVariant: 'small-caps'
-};
+document.addEventListener("DOMContentLoaded", function() {
 
-for (let s in h1Styles) {
-  h1.style[s] = h1Styles[s];
-}
+  const label = 'leonardo prates';
+  const head = document.head;
+  const title = document.createElement('title');
+  const titleText = document.createTextNode(label);
+  const link = document.createElement('link');
+  const h1 = document.createElement('h1');
+  const a = document.createElement('a');
+  const h1Styles = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    textAlign: 'center'
+  };
+  const aStyles = {
+    color: '#000000',
+    textDecoration: 'none',
+    borderBottom: '5px dotted #000',
+    fontFamily: 'monospace',
+    fontVariant: 'small-caps'
+  };
 
-for (let s in aStyles) {
-  a.style[s] = aStyles[s];
-}
+  for (let s in h1Styles) {
+    h1.style[s] = h1Styles[s];
+  }
 
-link.rel = 'icon';
-link.href = 'https://dummyimage.com/16x16/000/66ff91.png&text=LP';
-a.href = 'https://odran037.github.io';
-a.text = label;
+  for (let s in aStyles) {
+    a.style[s] = aStyles[s];
+  }
 
-title.appendChild(titleText);
-head.appendChild(title);
-head.appendChild(link);
-h1.appendChild(a);
-body.appendChild(h1);
+  link.rel = 'icon';
+  link.href = 'https://dummyimage.com/16x16/000/66ff91.png&text=LP';
+  a.href = 'https://odran037.github.io';
+  a.text = label;
+
+  title.appendChild(titleText);
+  head.appendChild(title);
+  head.appendChild(link);
+  h1.appendChild(a);
+  this.body.appendChild(h1);
+
+});
